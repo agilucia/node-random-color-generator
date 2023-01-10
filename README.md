723aeb2c99babaff85fa03bd8e93086673d2c2c1
Agnes Pilar <agnes.pilar@yahoo.de>
2023-01-10 16:39:30 +0100

    Add first working version

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Node.js Random Color Generator
+
+> Generate a block of approximately 31x9 # characters in a random or chosen color on the command line
+
+##Usage
+
+Generate random color:
+
+```bash
+$ node index.js
+(add color)
+$ node index.js
+(add different color)
+```
+
+Or, choose a color:
+
+```bash
+$ node index.js red
+(red)
+```
--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('hello');
+import chalk from 'chalk';
+
+import randomColor from 'randomcolor';
+
+console.log(process.argv[3]);
+console.log(process.argv[2]);
+
+const lum = process.argv[3];
+const col = process.argv[2];
+
+const color = randomColor({
+  luminosity: lum,
+  hue: col,
+});
+
+function colorAndLog(str) {
+  console.log(chalk.hex(color).bold(str));
+}
+
+if (color === undefined) {
+  colorAndLog(str);
+} else if (color) {
+  console.log(color);
+}
+
+colorAndLog(`##############################
+##############################
+##############################
+########              ########
+########   ${color}    ########
+########              ########
+##############################
+##############################
+##############################`);
